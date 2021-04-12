bd7ba7f99c7a48a7b46c282cae244006e57c723f
gananath-oss <ganaya2k17@gmail.com>
2021-04-12 23:43:19 +0530

    cal-function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-var  screen = document.getElementById('txtScreen1');
+var  screen1 = document.getElementById('txtScreen1');
+var screen2 = document.getElementById('txtScreen2');
 
 function setScreen1(num) {
-	if (screen.value != 0) {
-		screen.value += num;
-	} else {
-		screen.value = num;
+	if (screen1.value != 0 || screen1.value != '+'){
+		screen1.value += num;
+	} else if (screen1.value == 0) {
+		screen1.value = num;
 	}
-	console.log(screen);
+}
+
+function clear() {
+	screen2.value = screen1.value;
+	screen1.value = '';
+}
+
+function equal(eq) {
+	screen2.value = screen1.value;
+	screen1.value = eval(screen1.value);
 }
